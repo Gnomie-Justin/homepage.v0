23cc16bce88a170c6074afd5611f9560c5bfa302
Justin W <justinw@MacBookPro-841.lan>
2025-04-29 20:38:03 -0700

    Initial commit of GnomeHaus MVP

---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+export default function Contact() {
+    return (
+      <div style={{ fontFamily: "serif", padding: "2rem" }}>
+        <h1>Contact GnomeHaus</h1>
+        <p>Email: <a href="mailto:justin@gnome-haus.com" style={{ color: 'darkgreen' }}>justin@gnome-haus.com</a></p>
+        <p>Address: (Coming soon)</p>
+      </div>
+    );
+  }
+  
