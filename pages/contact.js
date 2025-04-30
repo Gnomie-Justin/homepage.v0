33bad4eb7787d91734822c182ea763bc5c7c8e41
Justin W <justinw@MacBookPro-848.lan>
2025-04-30 07:22:38 -0700

    Update contact page with business address

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,17 @@
 export default function Contact() {
-    return (
-      <div style={{ fontFamily: "serif", padding: "2rem" }}>
-        <h1>Contact GnomeHaus</h1>
-        <p>Email: <a href="mailto:justin@gnome-haus.com" style={{ color: 'darkgreen' }}>justin@gnome-haus.com</a></p>
-        <p>Address: (Coming soon)</p>
-      </div>
-    );
-  }
-  
+  return (
+    <div style={{
+      fontFamily: "serif",
+      padding: "2rem",
+      color: "#000" // 👈 makes all text black
+    }}>
+      <h1>Contact GnomeHaus</h1>
+      <p>
+        Email: <a href="mailto:justin@gnome-haus.com" style={{ color: 'darkgreen' }}>
+          justin@gnome-haus.com
+        </a>
+      </p>
+      <p>Address: GnomeHaus, 2108 N ST #8695, Sacramento, CA, 95816, USA</p>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,26 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div style={{ fontFamily: "serif", padding: "2rem", background: "#f9f7f3", minHeight: "100vh" }}>
-      <h1 style={{ fontSize: "2rem" }}>Now is the season of quiet craft and cunning construction.</h1>
+    <div style={{
+      fontFamily: "serif",
+      padding: "2rem",
+      background: "#f9f7f3",
+      minHeight: "100vh",
+      color: "#000" // font color
+    }}>
+      <h1 style={{ fontSize: "2rem" }}>
+        Now is the season of quiet craft and cunning construction.
+      </h1>
       <p style={{ marginTop: "1rem", maxWidth: "600px" }}>
         At GnomeHaus, we build brands with precision and pride so your brilliance shines in the spotlight.
       </p>
       <Link href="/contact" legacyBehavior>
-        <a style={{ color: 'darkgreen', textDecoration: 'underline', marginTop: "2rem", display: "inline-block" }}>
+        <a style={{
+          color: 'darkgreen',
+          textDecoration: 'underline',
+          marginTop: "2rem",
+          display: "inline-block"
+        }}>
           Contact Us
         </a>
       </Link>
